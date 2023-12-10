bd4e2c0f0f4e0a9ef9a1cb058cf717226dac0d31
majkel99 <56017309+majkel99@users.noreply.github.com>
2023-12-10 19:40:12 +0100

    add views

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+# Importing required libs
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+# Home route
 @app.route("/")
-def home():
-    return "Hello World!,123"
+def main():
+    return render_template("index.html")
+
+# Prediction route
+@app.route('/prediction', methods=['POST'])
+def predict_image_file():
+    return render_template("result.html")
